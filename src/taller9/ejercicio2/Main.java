51b3094797177885a511abef755af897907f36b9
Gabriela Porras <gporrasb@unicartagena.edu.co>
2026-05-27 14:15:59 -0500

    feat(taller9-ejercicio2): invocacion de clase base desde Pez

---
+++ src/taller9/ejercicio2/Main.java
@@ -0,0 +1,8 @@
+package taller9.ejercicio2;
+
+public class Main {
+    public static void main(String[] args) {
+        Pez pez = new Pez("Salmon", "Agua dulce");
+        pez.mostrarEspecie();
+    }
+}
