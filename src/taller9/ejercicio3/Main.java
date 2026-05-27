5f10053021c96d39142bbe99265ccca492e1dca8
Gabriela Porras <gporrasb@unicartagena.edu.co>
2026-05-27 14:20:01 -0500

    feat(taller9-ejercicio3): demostracion teorica errores de super

---
+++ src/taller9/ejercicio3/Main.java
@@ -0,0 +1,9 @@
+package taller9.ejercicio3;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Errores de compilacion al usar super:");
+        System.out.println("1. Intentar utilizar super en un contexto que no sea una clase derivada.");
+        System.out.println("2. Intentar invocar un atributo privado de la clase base utilizando super.");
+    }
+}
