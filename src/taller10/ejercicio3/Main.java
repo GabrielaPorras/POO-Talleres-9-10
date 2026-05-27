f63508beef05a464aacd17b84f57d6c5a6d081a7
Gabriela Porras <gporrasb@unicartagena.edu.co>
2026-05-27 14:28:51 -0500

    feat(taller10-ejercicio3): demostracion teorica errores de sobrescritura

---
+++ src/taller10/ejercicio3/Main.java
@@ -0,0 +1,9 @@
+package taller10.ejercicio3;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Problemas al intentar sobrescribir metodos incorrectamente:");
+        System.out.println("1. Cambiar la firma del metodo genera sobrecarga, no sobrescritura.");
+        System.out.println("2. No utilizar @Override es una mala practica porque el compilador no alertara si hay errores al sobrescribir.");
+    }
+}
